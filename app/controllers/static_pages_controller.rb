783f3ebd21ee7a2a87458b8037dfd98d0b79e627
g_ryotaro <goto@basicinc.jp>
2015-08-01 20:56:28 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  skip_filter :authenticate_user!
   def home
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:show]
+  before_action :correct_user, only: [:show]
+
   def show
   end
 
   def index
+    @users = User.all.page(params[:page])
   end
+
+  private
+    def signed_in_user
+      redirect_to new_user_session_path unless user_signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user == @user
+    end
 end
